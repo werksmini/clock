a6c6613b89100a5c9baad063011d977eeca50214
parker <parkermusselman@gmail.com>
2025-06-10 20:52:34 -0400

    Testing

--- time.py
+++ time.py
@@ -3,6 +3,8 @@
 import sys
 import os
 
+#This is a test
+
 # Setup directories
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
