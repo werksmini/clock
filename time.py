9fcf898701a3cfd871ac20f001d3b18140dc9180
werksmini <60582536+werksmini@users.noreply.github.com>
2025-09-30 22:24:07 -0400

    Update time.py

--- time.py
+++ time.py
@@ -29,10 +29,10 @@ GPIO.setup(SWITCH_PIN_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # --- Font profiles for each switch position ---
 font_profiles = {
-    (0, 1): {"mode": "inverted", "font": "DS-DIGIT.TTF", "size": 300}, #position 3
-    (0, 0): {"mode": "inverted", "font": "Roboto-Regular.ttf", "size": 250}, #position 1
+    (0, 1): {"mode": "inverted", "font": "DS-DIGIT.TTF", "size": 250}, #position 3
+    (0, 0): {"mode": "inverted", "font": "Roboto-Regular.ttf", "size": 220}, #position 1
     (1, 0): {"mode": "inverted", "font": "WDXLLubrifontJPN-Regular.ttf", "size": 240},   #position 2
-    (1, 1): {"mode": "inverted", "font": "Outfit-Regular.ttf", "size": 270},  #position 4
+    (1, 1): {"mode": "inverted", "font": "Outfit-Regular.ttf", "size": 200},  #position 4
 }
 
 # Available Fonts:
